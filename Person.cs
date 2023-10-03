ef5e2a6629412b2ef99c83d81e953c4a4fa8f09e
VladimirKardashin <kardv@yandex.ru>
2023-10-03 19:49:06 +0300

    Исходник person

---
+++ Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinOopApp
+{
+    public class Person
+    {
+        /*
+        public string FirstName;
+        public string MiddleName;
+        public string LastName;
+        */
+
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string middleName, string lastName) 
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
+        }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = "";
+            LastName = lastName;
+        }
+
+        public string GetFullName()
+        {
+            string fullName = FirstName;
+            if (MiddleName.Length > 0) fullName += " " + MiddleName;
+            fullName += " " + LastName;
+            return fullName;
+        }
+
+        
+
+        // Домашняя работа 1_8
+        // Функция возвращает Фамилия И.О.
+        public string GetShortName()
+        {
+            string shortName = LastName;
+            // char[] fN = FirstName.ToCharArray();
+            char fN = FirstName[0];
+            // char[] mN = MiddleName.ToCharArray();
+            char mN = MiddleName[0];
+            // shortName += " " + fN[0] + "." + mN[0] + ".";
+            shortName += " " + fN + "." + mN + ".";
+            return shortName;
+        }
+        
+
+        public virtual string GetInfo()
+        {
+            string info = GetFullName();
+            return info;
+        }
+    }
+}
